c3db119e907c30e889e2487e7715b461ba0688d9
Bruno Schwarzentruber <bruno.schwarzentruber@i11.ch>
2019-11-15 19:09:44 +0100

    new Class F1a und F2a in ff1

---
+++ GitTest_00/ff1/F1a.java
@@ -0,0 +1,10 @@
+package ff1;
+
+public class F1a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ GitTest_00/ff1/F2a.java
@@ -0,0 +1,10 @@
+package ff1;
+
+public class F2a {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
